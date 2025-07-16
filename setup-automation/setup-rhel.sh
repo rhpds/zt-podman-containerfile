1ffd386d3237ef4fd019f248aa1b0b504901eca5
Matt Micene (RHT) <mmicene@redhat.com>
2025-07-16 14:58:13 -0400

    Update to RHEL 10 for base and container, move to standard user, minor edits

--- setup-automation/setup-rhel.sh
+++ setup-automation/setup-rhel.sh
@@ -1,47 +1,26 @@
 !#/bin/bash
-LOG=/dev/null
 
-while [ ! -f /opt/instruqt/bootstrap/host-bootstrap-completed ]
-do
-   echo "Waiting for Instruqt to finish booting the VM"
-   sleep 1
-done
+# Pull the UBI image to avoid during lab time
+podman pull registry.access.redhat.com/ubi10/ubi
 
-yum remove -y google-rhui-client-rhel8.noarch
-yum clean all
-subscription-manager config --rhsm.manage_repos=1
-subscription-manager register --activationkey=${ACTIVATION_KEY} --org=12451665 --force
-
-touch $LOG
-
-echo "Installing Podman" >> $LOG
-dnf -y install container-tools
-podman pull registry.access.redhat.com/ubi9/ubi
-mkdir -p ~root/my-container/
-cat << EOF >> ~root/my-container/Containerfile
-FROM registry.access.redhat.com/ubi9/ubi
+# Create the example containerfile
+mkdir -p ~/my-container/
+cat << EOF >> ~/my-container/Containerfile
+FROM registry.access.redhat.com/ubi10/ubi
 RUN dnf -y install httpd
 EXPOSE 80
 CMD ["/usr/sbin/httpd","-DFOREGROUND"]
 EOF
 
+# Create the index.html
+# moved from step 2 setup script
 
-#Create a done file to signal we have finished
-touch ${LOG}.done
-n=1
-GREEN='\033[0;32m' 
-NC='\033[0m' # No Color
-
-while [ ! -f ${LOG}.done ] ;
-do
-      if test "$n" = "1"
-      then
-	    clear
-            n=$(( n+1 ))	 # increments $n
-      else
-	    printf "."
-      fi
-      sleep 2
-done
-clear
-echo -e "${GREEN}Ready to start your scenario${NC}"
+mkdir -p ~/my-container/app
+cat << EOF >> ~/my-container/app/index.html
+<HTML>
+<HEAD>
+<TITLE>My Web App</TITLE>
+</HEAD>
+This is my web app
+</HTML>
+EOF
