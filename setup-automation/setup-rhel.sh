aa5ac1b2ee0e29f0d699de1335a51a15ccabe205
Matt Micene (RHT) <mmicene@redhat.com>
2025-07-21 09:56:26 -0400

    Try cloning repo instead of sh/cat

--- setup-automation/setup-rhel.sh
+++ setup-automation/setup-rhel.sh
@@ -4,24 +4,28 @@
 podman pull registry.access.redhat.com/ubi10/ubi
 podman pull registry.access.redhat.com/ubi10/ubi-init
 
-# Create the starting containerfile
-mkdir -p ~/my-container/
-cat << EOF >> ~/my-container/Containerfile
-FROM registry.access.redhat.com/ubi10/ubi
-RUN dnf -y install httpd
-EXPOSE 80
-CMD ["/usr/sbin/httpd","-DFOREGROUND"]
-EOF
+git clone --single-branch --branch clone_examples https://github.com/rhpds/zt-podman-containerfile.git /tmp
+cp -r /tmp/zt-podman-containerfile/files /home/rhel
+chown rhel:rhel /home/rhel/*
 
-# moved from step 2 setup script
-# Create the index.html
-mkdir -p ~/my-container/app
-cat << EOF >> ~/my-container/app/index.html
-<HTML>
-<HEAD>
-<TITLE>My Web App</TITLE>
-</HEAD>
-This is my web app
-</HTML>
-EOF
+# Create the starting containerfile
+#mkdir -p ~/my-container/
+#cat << EOF >> ~/my-container/Containerfile
+#FROM registry.access.redhat.com/ubi10/ubi
+#RUN dnf -y install httpd
+#EXPOSE 80
+#CMD ["/usr/sbin/httpd","-DFOREGROUND"]
+#EOF
+#
+## moved from step 2 setup script
+## Create the index.html
+#mkdir -p ~/my-container/app
+#cat << EOF >> ~/my-container/app/index.html
+#<HTML>
+#<HEAD>
+#<TITLE>My Web App</TITLE>
+#</HEAD>
+#This is my web app
+#</HTML>
+#EOF
 
